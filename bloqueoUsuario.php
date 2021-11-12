a762610175441556c6ce12e5847e392da00c6d93
Jose Antonio Marquez <joseantoniomarquezglez@gmail.com>
2021-11-12 18:07:03 +0100

    IMPLEMENTACION BLOQUEO USUARIO LOGIN Y LOGOUT

--- bloqueoUsuario.php
+++ bloqueoUsuario.php
@@ -1,48 +1,40 @@
 <?php
-
-
 session_start();
 
+$correo = $_SESSION["correo"];
+$intentos = $_SESSION["Usuario_numero_intentos"];
+$_SESSION["correo_valido"] = 0;
+if ($intentos < 3) {
+    $_SESSION["password_error"] = 1;
+} else {
+    $_SESSION["password_error"] = 0;
+}
 
+$_SESSION["correoRegistrado"] = 0;
+$_SESSION["variableBandera"] = 0;
 
-
-        $correo=$_SESSION["correo"];
-        $intentos=$_SESSION["Usuario_numero_intentos"];
-        $_SESSION["correo_valido"]=0;
-        if ($intentos<3) {
-            $_SESSION["password_error"]=1;
-        }else{
-            $_SESSION["password_error"]=0;
-        }
-        
-        $_SESSION["correoRegistrado"]=0;
-        $_SESSION["variableBandera"]=0;
-
-      $conexion = mysqli_connect("localhost", "root", "", "usuarios") or
+$conexion = mysqli_connect("localhost", "root", "", "usuarios") or
     die("Problemas con la conexion");
 
-    $intentos++;
+$intentos++;
 
-    
+if ($intentos <= 3) {
+    $consulta = mysqli_query($conexion, "UPDATE usuarios SET Usuario_numero_intentos='$intentos' WHERE Usuario_email= '$correo'") or
+        die("Problemas en el select:" . mysqli_error($conexion));
+    $_SESSION["Usuario_numero_intentos"] = $intentos;
 
-    if ($intentos<=3) {
-        $consulta = mysqli_query($conexion, "UPDATE usuarios SET Usuario_numero_intentos='$intentos' WHERE Usuario_email= '$correo'") or
-    die("Problemas en el select:" . mysqli_error($conexion));
-    $_SESSION["Usuario_numero_intentos"]=$intentos;
-    } else {
-
-        if ($_SESSION["Usuario_bloqueado"]==0) {
+    if ($intentos == 3) {
+        if ($_SESSION["Usuario_bloqueado"] == 0) {
             $fechaActual = date('Y-m-d');
             $consulta = mysqli_query($conexion, "UPDATE usuarios SET Usuario_bloqueado='1', Usuario_fecha_bloqueo='$fechaActual' WHERE Usuario_email= '$correo'") or
-        die("Problemas en el select:" . mysqli_error($conexion));
-        $_SESSION["Usuario_numero_intentos"]=0;
-        $_SESSION["Usuario_bloqueado"]=1;
-
+                die("Problemas en el select:" . mysqli_error($conexion));
+            $_SESSION["Usuario_numero_intentos"] = 0;
+            $_SESSION["Usuario_bloqueado"] = 1;
         }
-
     }
+}
 
 $url = "./";
-      header("HTTP/1.1 301 Moved Permanently");
-      header("Location: ".$url);
-      exit();
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: " . $url);
+exit();
--- login/borrado.php
+++ login/borrado.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
+
+$correo = $_SESSION["correo"];
+
+$fechaActual = date('Y-m-d');
+$conexion = mysqli_connect("localhost", "root", "", "usuarios") or
+    die("Problemas con la conexion");
+    $consulta = mysqli_query($conexion, "UPDATE usuarios SET Usuario_numero_intentos='0',Usuario_fecha_ultima_conexion='$fechaActual' WHERE Usuario_email= '$correo'") or
+    die("Problemas en el select:" . mysqli_error($conexion));
 session_unset();
 session_destroy();
+
 $url = "../";
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: ".$url);
